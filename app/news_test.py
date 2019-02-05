7e90fdeeab13bb09b1f8e00b4e1df8e8378a9cd4
blackrose <wahomekelving@gmail.com>
2019-02-05 11:12:56 +0300

    Added a news test class to test behaviour of the news class

---
+++ app/news_test.py
@@ -0,0 +1,12 @@
+import unittest
+from models import news
+News = news.News
+
+class NewsTest (unittest.TestCase):
+    '''
+    Test class to test the behaviour of the News Class
+    '''
+
+    def setUp(self):
+        '''
+        
