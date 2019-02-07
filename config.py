628a051e701095479b2ba39bf10bfab7fedb1d1f
blackrose <wahomekelving@gmail.com>
2019-02-07 11:41:45 +0300

    Deployment to heroku

--- config.py
+++ config.py
@@ -9,6 +9,8 @@ class Config:
     NEWS_BASE_API_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey=89d86575840d4357be582bd2faf0f4ae'
     SOURCE_NEWS_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey=89d86575840d4357be582bd2faf0f4ae'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('HOMES')
+
 
 
 class ProdConfig(Config):
